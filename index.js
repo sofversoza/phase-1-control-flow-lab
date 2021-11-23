c70705620bfc992b6e919bf3f94e9e2517f03b93
sofiaversoza <sofversoza@gmail.com>
2021-11-22 18:57:02 -0700

    commit tree f94be771af19cc0bdcf8060a3a7aeb3d909e1d4e

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) 
+    return 'This one is on me!';
+  else if (distance > 2500)
+    return 'No can do.';
+  else if (distance > 2000) 
+    return 'I will gladly take your thirty bucks.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  const mssg = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return mssg;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  let message;
+  switch (tip) {
+    case 'generous':
+      message = "Thank you so much.";
+      break;
+    case 'not as generous':
+      message = "Thank you.";
+      break;
+    default:
+      message = "Bye.";  
+  }
+  return message;
 }
